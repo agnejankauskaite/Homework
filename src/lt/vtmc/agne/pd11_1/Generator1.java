821b78c42de06de1a62301f53baea6a832a54c0b
pekla <pekla@agne>
2020-10-05 14:54:42 +0300

    2020 10 05  14:54

---
+++ src/lt/vtmc/agne/pd11_1/Generator1.java
@@ -0,0 +1,15 @@
+package lt.vtmc.agne.pd11_1;
+
+public class Generator1 {
+
+	private int[] luckyNumbers;
+	
+	public int[] generateNumbers(int arraySize) {
+		this.luckyNumbers = new int[arraySize];
+		for (int i = 0; i< luckyNumbers.length; i++) {
+			luckyNumbers[i] = (int)(Math.random() * 100);
+		}
+		return luckyNumbers;
+	}
+
+}
---
+++ src/lt/vtmc/agne/pd11_1/Tester1.java
@@ -0,0 +1,13 @@
+package lt.vtmc.agne.pd11_1;
+
+import java.util.Arrays;
+
+public class Tester1 {
+
+	public static void main(String[] args) {
+		Generator1 generate = new Generator1();
+		generate.generateNumbers(5);
+		System.out.println(Arrays.toString(generate.generateNumbers(5)));
+	}
+
+}
